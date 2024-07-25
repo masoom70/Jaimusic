5022f7762d182638aa00c295969fb475be628630
vicky0604hello <166818922+vicky0604hello@users.noreply.github.com>
2024-07-25 14:03:34 +0530

    Update config.py
    Signed-off-by: vicky0604hello <166818922+vicky0604hello@users.noreply.github.com>

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID" None))
+API_ID = int(getenv("API_ID", 6435225))
 API_HASH = getenv("API_HASH")
 
 # Get your token from @BotFather on Telegram.
